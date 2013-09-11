e2902771b1cd4cd2134dd7e429e6740433063ba9
isim <ihcsim@gmail.com>
2013-09-10 20:52:45 -0700

    Remove throws clause from constructor.

--- src/main/java/isim/orion/jms/ptp/sync/Producer.java
+++ src/main/java/isim/orion/jms/ptp/sync/Producer.java
@@ -14,24 +14,24 @@ public class Producer {
   private Channel channel;
   private String queue;
   
-  public Producer(Channel channel, String queue) throws IOException{
+  public Producer(Channel channel, String queue) {
     this.channel = channel;
     this.queue = queue;
   }
   
-  public void sendMessage(String message) throws IOException{
+  public void sendMessage(String message) throws IOException {
     if(message == null)
       throw new IllegalArgumentException();
     channel.basicPublish("", queue, null, message.getBytes());
   }
   
-  public boolean isConnected(){
+  public boolean isConnected() {
     if(channel == null)
       return false;
     return channel.isOpen();
   }
   
-  public void disconnect() throws IOException{
+  public void disconnect() throws IOException {
     if(channel != null)
       channel.close();
   }
